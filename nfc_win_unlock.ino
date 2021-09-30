8bfa693c07ee28203de9b8e82b699c1f4dd799d3
iberotecno <info@iberotecno.com>
2021-10-01 00:13:04 +0200

    Update nfc_win_unlock.ino
    Code documentation

--- nfc_win_unlock.ino
+++ nfc_win_unlock.ino
@@ -1,38 +1,38 @@
 /*
-   Programa que desbloquea el PC por medio de un tag NFC.
-   
+   Programa que introduce por teclado el PIN de desbloqueo de Windows al detectar el tag NFC correcto.
+   Cuando detecta la tarjeta NFC correcta, introduce una contraseña que puede ser la del correo, KeePass, etc.
+
    Código fuente desarrollado por iberotecno. Año 2021.
    Versión 1.0
-   
+
    http://iberotecno.com
-   http://duino.pro
 
    MIT License
 
-Copyright (c) 2021 iberotecno.com
+  Copyright (c) 2021 iberotecno.com
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
 
 
-Contact with info@iberotecno.com to ask for permissions.
-Have a written contract from iberotecno.
+  Contact with info@iberotecno.com to ask for permissions.
+  Have a written contract from iberotecno.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 
  * */
 
@@ -42,12 +42,16 @@ SOFTWARE.
 #include <NfcAdapter.h>
 #include <Keyboard.h>
 
+//inicializamos el PN535 en modo i2c
 PN532_I2C pn532i2c(Wire);
 PN532 nfc(pn532i2c);
 
 void setup() {
+
   Serial.begin(115200);
   Serial.println("Iniciando...");
+
+  //inicializamos lector NFC
   nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
@@ -55,32 +59,29 @@ void setup() {
     while (1); // halt
   }
 
-  // Datos leídos, mostrarlos.
+  // Lector NFC detectado. Mostrar versión del firmware.
   Serial.print("Encontrado chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
   Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
-
   nfc.setPassiveActivationRetries(0xFF);
   nfc.SAMConfig();
-
   Serial.println("Esperando leer codigo");
   Keyboard.begin();
 }
 
-
-
 void loop() {
-  // Look for new cards
+
+  // Detectando posibles tarjetas
   boolean success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer para almacenar el UID leído
-  uint8_t uidLength;                        // Longitud del UID 
-
+  uint8_t uidLength;                        // Longitud del UID
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
+  // Si detectamos una tarjeta o tag NFC mostraremos su UID
   if (success) {
     Serial.println("Tarjeta detectada");
-    Serial.print("UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
-    Serial.print("UID Value: ");
+    Serial.print("Longitud del UID: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+    Serial.print("Valor del UID: ");
     for (uint8_t i = 0; i < uidLength; i++)
     {
       Serial.print(" 0x"); Serial.print(uid[i], HEX);
@@ -96,35 +97,40 @@ void loop() {
   }
 
 
-  // ESTABLECER AQUÍ TU UID (lo verás en el monitor serie al acercar un llavero o tarjeta)
+  // ESTABLECER AQUÍ TU UID DEL LLAVERO (lo verás en el monitor serie al acercar un llavero)
   if (uid[0] == 0x29 &&
       uid[1] == 0xA9 &&
       uid[2] == 0xA0 &&
       uid[3] == 0x2) {
     Serial.println("UID Correcto");
-   
-   // ESTABLECER AQUÍ UN SEGUNDO UID, OPCIONAL (lo verás en el monitor serie al acercar un llavero o tarjeta)
+
+    // ESTABLECER AQUÍ EL PIN DE DESBLOQUEO DE WINDOWS
+    Serial.println("Escribiendo el PIN del PC...");
+    Keyboard.print(" "); // Simulamos presionar la tecla espacio para despertar el PC
+    delay(1000);
+    Keyboard.print("1234"); //Cambiar este número por tu PIN de Windows
+    delay(1000);
+    Keyboard.write(10);
+
+
+    // ESTABLECER AQUÍ TU UID DE LA TARJETA (lo verás en el monitor serie al acercar una tarjeta)
   }
   else if (uid[0] == 0xE9 &&
            uid[1] == 0x41 &&
            uid[2] == 0xC5 &&
            uid[3] == 0x56) {
     Serial.println("UID Correcto");
+
+    // ESTABLECER AQUÍ LA CONTRASEÑA DEL KEEP PASS, CORREO, ETC...
+    Serial.println("Escribiendo contraseña de la APP...");
+    Keyboard.print("PASS"); //Cambiar esto por tu contraseña habitual
+    delay(1000);
+    Keyboard.write(10);
   }
   else {
-    Serial.println("UID Incorrecto"); // Si no se detecta una tarjeta o llavero o es incorrecto su UID saldrá este texto de forma contínua en el monitor serie. Es normal.
+    Serial.println("UID Incorrecto"); // Si no se detecta una tarjeta o llavero o es incorrecto su UID saldrá este texto.
     //delay(500);
     return;
   }
-
-
- // ESTABLECER AQUÍ LA CONTRASEÑA DEL PC
-  Serial.println("Escribiendo contraseña del PC...");
-  Keyboard.print(" "); // Simulamos presionar la tecla espacio
-  delay(1000);
-  Keyboard.print("1234");
-  delay(1000);
-  Keyboard.write(10);
-
   delay(500);
 }
